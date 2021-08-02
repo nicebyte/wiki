4dcdfd5f549e1a68e81d4cd0d1c6ff2e2593f5ed
nicebyte <12521759+nicebyte@users.noreply.github.com>
2021-08-01 19:08:34 -0700

    Add search

--- Templates/Footer.sh
+++ Templates/Footer.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cat << EOF
+<hr/>
+<script async src="https://cse.google.com/cse.js?cx=bc9a54b74664cc240"></script>
+<div class="gcse-search"></div>
   </body>
 <!-- Markdeep: --><style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style><script src="/WebAssets/markdeep.min.js" charset="utf-8"></script><script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>
 
