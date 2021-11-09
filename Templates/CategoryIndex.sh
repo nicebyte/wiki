00bb0205fe41d191b9f7347570736e3511afbce5
nicebyte <nicebyte@gpfault.net>
2021-11-08 19:54:24 -0800

    Update some templates

--- Templates/CategoryIndex.sh
+++ Templates/CategoryIndex.sh
@@ -3,7 +3,9 @@ source Templates/Header.sh
 cat <<EOF
 **Articles in Category ${DOCUMENT_TITLE}**
 
+<div style = "column-count: 3; column-gap: 1em; column-rule-style: solid">
 ${DOCUMENT_BODY}
+</div>
 EOF
 source Templates/Footer.sh
 
--- Templates/Footer.sh
+++ Templates/Footer.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cat << EOF
-<hr/>
-<script async src="https://cse.google.com/cse.js?cx=bc9a54b74664cc240"></script>
-<div class="gcse-search"></div>
-  </body>
+</body>
 <!-- Markdeep: --><style class="fallback">body{visibility:hidden;white-space:pre;font-family:monospace}</style><script src="/WebAssets/markdeep.min.js" charset="utf-8"></script><script>window.alreadyProcessedMarkdeep||(document.body.style.visibility="visible")</script>
 
 </html>
